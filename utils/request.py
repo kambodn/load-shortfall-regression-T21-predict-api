61cfa476b8c0233ad4c029a31207aaa2ca038002
David Kambo <kambodn@gmail.com>
2022-05-27 13:42:34 +0300

    Update request.py

--- utils/request.py
+++ utils/request.py
@@ -36,7 +36,7 @@ feature_vector_json = test.iloc[1].to_json()
 # NOTE: When testing your instance of the API on a remote machine
 # replace the URL below with its public IP:
 
-url = 'http://54.171.195.138:5000/api_v0.1'
+url = 'http://54.247.39.193:5000/api_v0.1'
 # url = 'http://127.0.0.1:5000/api_v0.1'
 
 # Perform the POST request.
